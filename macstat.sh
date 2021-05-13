7eb22dfcda20f467ab1b87e12bb07bc25d3cb8e0
jo-hannes <jo-hannes@dev-urandom.de>
2021-05-13 12:48:16 +0200

    Add option to colour values on warning/error

--- macstat.sh
+++ macstat.sh
@@ -8,21 +8,29 @@
 function help
 {
     cat <<HelpText
-Usage: $(basename "${0}") [-bh]
+Usage: $(basename "${0}") [-bch]
 
 Display important system statistics of macOS
     -b  Display bars
+    -c  Coloured output
     -h  Display help
 HelpText
 }
 
 # configuration
 # currently we need to specify the device for measuring hdd usage
-cfgMacHddUsageDev="/dev/disk1s1"
+readonly cfgMacHddUsageDev="/dev/disk1s1"
+
+# colors
+readonly COL_WARN='\033[1;33m'  # Warnings in yellow
+readonly COL_ERR='\033[0;31m'   # Errors in red
+readonly COL_NC='\033[0m'       # No Color
 
 # measuremnt configuration
 cfgDescription=("CPU"      "RAM"      "HDD space"          "HDD inode"          "Batt"         "Batt Keyboard"        "Batt Mouse"        "TM Backup Age")
    cfgFunction=(cpuPercent ramPercent hddSpaceUsagePercent hddInodeUsagePercent batteryPercent batteryKeyboardPercent batteryMousePercent tmBackupAgeSeconds)
+       cfgWarn=(80         70         75                   20                   20             20                     20                   86400)
+      cfgError=(90         80         85                   30                   10             10                     10                  172800)
 
 
 function cpuPercent
@@ -121,9 +129,12 @@ function printBar
 function printPercent
 {
     if [[ -n $2 ]]; then
-        printf "%-${descLen}s: %3d%%" "$1" "$2"
+        printf "%-${descLen}s: $3%3d%%" "$1" "$2"
         if [[ ${bars} -eq 1 ]]; then
             printBar "$2"
+        fi
+        if [[ -n "$3" ]]; then
+            echo -ne "${COL_NC}"
         fi
         printf "\n"
     fi
@@ -142,14 +153,18 @@ function printTimeSpan
         days="$(( span / 60 / 60 / 24 ))"
         hours="$(( ( span / 60 /60 ) % 24 ))"
         mins="$(( ( span / 60 ) % 60 ))"
-        printf "%-${descLen}s: " "$1"
+        printf "%-${descLen}s: $3" "$1"
         if [[ ${days} -gt 0 ]]; then
             echo -n "${days}d"
         fi
         if [[ ${hours} -gt 0 || ${days} -gt 0 ]]; then
             printf "%02dh" "${hours}"
         fi
-        printf "%02dm\n" "${mins}"
+        printf "%02dm" "${mins}"
+        if [[ -n "$3" ]]; then
+            echo -ne "${COL_NC}"
+        fi
+        printf "\n"
     fi
 }
 
@@ -157,10 +172,31 @@ function printAll
 {
     for idx in ${!cfgFunction[*]}
     do
+        txtCol=""
+        if [[ ${color} -eq 1 ]]; then
+            # check for warnings/error
+            if [[ "${cfgError[$idx]}" -ge "${cfgWarn[$idx]}" ]]; then
+                # higher values are problematic
+                if [[ "${value[$idx]}" -ge "${cfgError[$idx]}" ]]; then
+                    txtCol="${COL_ERR}"
+                elif [[ "${value[$idx]}" -ge "${cfgWarn[$idx]}" ]]; then
+                    txtCol="${COL_WARN}"
+                fi
+            else
+                # lower values are problematic
+                if [[ "${value[$idx]}" -le "${cfgError[$idx]}" ]]; then
+                    txtCol="${COL_ERR}"
+                elif [[ "${value[$idx]}" -le "${cfgWarn[$idx]}" ]]; then
+                    txtCol="${COL_WARN}"
+                fi
+            fi
+        fi
+
+        # print it
         if [[ "${value[$idx]}" -gt 100 ]]; then
-            printTimeSpan "${cfgDescription[idx]}" "${value[$idx]}"
+            printTimeSpan "${cfgDescription[idx]}" "${value[$idx]}" "${txtCol}"
         else
-            printPercent "${cfgDescription[idx]}" "${value[$idx]}"
+            printPercent "${cfgDescription[idx]}" "${value[$idx]}" "${txtCol}"
         fi
     done
 }
@@ -170,13 +206,17 @@ function main
 {
     # parse options
     bars=0
-    while getopts bh opt; do
+    color=0
+    while getopts bch opt; do
         case $opt in
         b)
             bars=1
             local colLen
             colLen=$(tput cols)
             barLen=$(( colLen - descLen - 9 ))
+            ;;
+        c)
+            color=1
             ;;
         h)
             help
